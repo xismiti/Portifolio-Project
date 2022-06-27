fa7cb45c8a2e73145e0f214292958603bcc9d9b4
Guilherme Schmidt <Gui.m.schmidt@gmail.com>
2022-06-27 17:49:57 -0300

    nav update

--- js/index.js
+++ js/index.js
@@ -8,3 +8,15 @@ function parallax(e){
 
   });
 }
+
+const links = document.querySelectorAll(".scroll_to");
+
+links.forEach((item)=> {
+  item.addEventListener("click", () => {
+
+      const el = document.getElementById(item.getAttribute("data-link"))
+      el.scrollIntoView({behavior:"smooth", block:"center"})
+    
+
+  })
+})
